116a1107cc2a13649bfb1c1bdfe5733c722eeb99
Kunwoo Kim <aidengoldkr@gmail.com>
2026-02-02 13:33:03 +0900

    Update home.jsx

--- aiden-web/src/home.jsx
+++ aiden-web/src/home.jsx
@@ -235,7 +235,8 @@ function Home({ useParticlesHook, projectData, skillData, educationList, awardLi
           <div className='exter-grid'>
             <div
               className='exter-card exter-card--portfolio'
-              onClick={() => navigate("/portfolio")}
+              onClick={() => {window.scrollTo({ top: 0, behavior: "instant" });
+																								navigate("/portfolio")}}
             >
               <div className='exter-card-title'>{t.portfolioTitle}</div>
               <div className='exter-card-desc'>{t.portfolioDesc}</div>
